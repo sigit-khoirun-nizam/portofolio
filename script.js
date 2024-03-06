2003e880ab7375ec1522464d8292c3eb3a678cd8
[sigit-khoirun-nizam] <[sigitkhoirunnizam10@gmail.com]>
2024-03-06 10:05:09 +0700

    Initial Add

---
+++ script.js
@@ -0,0 +1,20 @@
+// Toogle 
+const navSlide = () => {
+    const burger = document.querySelector(".burger")
+    const navList = document.querySelector("nav")
+
+    burger.addEventListener("click", () => {
+        navList.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+}
+
+navSlide()
+
+// Clear form
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")){
+        form.reset();
+    }
+};
+
